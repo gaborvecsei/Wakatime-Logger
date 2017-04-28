77b303bb1ee4f3ec637ee6f475e888bb4d628ab5
Gabor Vecsei <vecseigabor.x@gmail.com>
2017-04-28 19:21:29 +0200

    init

---
+++ README.md
@@ -0,0 +1,47 @@
+# Wakatime logger
+
+:sunglasses: :sunglasses: :sunglasses:
+
+## TL;DR
+
+"Extend" the *Wakatime Free Plan* so you won't loose your logged data!
+
+## Info
+
+This is a Python 3.5 script which you can use to log you times from *Wakatime*.
+
+*Wakatime* offers that with a `Free Plan` you can access your logged times for `1 week`.
+Unfortunately this means that your times will be lost after a week.
+So this is why I made this scrip. If you run it at least `once a week` you will have **ALL** your logged times.
+Of course, you can set up to run the script automatically when your computer starts, etc... (Use your imagination) :stuck_out_tongue_winking_eye:.
+
+## Usuage
+
+Just update the `config.ini` file with your `Api Key` and the `start date` for the time logger.
+
+The start date is used for the first run.
+Don't worry if you don't remember the first date for the logging,
+it will recognize if a date is "valid", so it has time information stored.
+
+Than just run it: `python wakatime_logger.py`
+
+## TODO
+
+- saved data visualization
+
+## Disclaimer
+
+I do not encourage anybody to use this scrip instead of a purchase for a Wakatime plan.
+Your can upgrade the plan [here](https://wakatime.com/).
+This is only an awesome project
+for the usage of the Wakatime API. :v:
+
+## About
+
+Gábor Vecsei
+
+- [Personal Blog](https://gaborvecsei.wordpress.com/)
+- [LinkedIn](https://www.linkedin.com/in/gaborvecsei)
+- [Twitter](https://twitter.com/GAwesomeBE)
+- [Github](https://github.com/gaborvecsei)
+- vecseigabor.x@gmail.com
---
+++ wakatime_logger.py
@@ -0,0 +1,76 @@
+import requests
+import base64
+from datetime import timedelta, date, datetime
+import pandas as pd
+import os
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+FILE_NAME = config.get("Waka", "fileName")
+API_KEY = config.get("Waka", "apiKey")
+BASE_URL = config.get("Waka", "baseUrl")
+START_DATE = datetime.strptime(config.get("Waka", "startDate"), "%Y-%m-%d").date()
+
+
+def prepare_request_header(api_key_in_bytes):
+    b64_api_key = base64.b64encode(api_key_in_bytes).decode("utf-8")
+    headers = {'content-type': 'application/json', 'Authorization': 'Basic ' + b64_api_key}
+    return headers
+
+
+def get_durations_from_waka(date, header):
+    req_url = BASE_URL + date.strftime("%Y-%m-%d")
+    response = requests.get(req_url, headers=header)
+    return response.json()
+
+
+def date_range(start_date, end_date):
+    for n in range(int((end_date - start_date).days)):
+        yield start_date + timedelta(n)
+
+
+def write_data_to_dataframe(df, start_date, end_date):
+    last_df_index = len(df)
+    if last_df_index > 0:
+        start_date_str = df["date"].values[last_df_index - 1]
+        start_date = datetime.strptime(start_date_str, "%Y-%m-%d").date()
+        start_date = start_date + timedelta(days=1)
+    for d in date_range(start_date, end_date):
+        response_json = get_durations_from_waka(d, prepare_request_header(str.encode(API_KEY)))
+        try:
+            data = response_json["data"]
+            data_dict = {}
+            for duration_data in data:
+                project_name = duration_data["project"]
+                duration = duration_data["duration"]
+                try:
+                    data_dict[project_name] += duration
+                except KeyError:
+                    data_dict[project_name] = duration
+            for k, v in data_dict.items():
+                new_row = [d.strftime("%Y-%m-%d"), k, v]
+                df.loc[last_df_index] = new_row
+                last_df_index += 1
+        except KeyError as keyError:
+            print("[*] ERROR: for {0} key error: {1}".format(d.strftime("%Y-%m-%d"), keyError))
+            print("This means that you can't see this day's records because you've exceeded the free Waka limit")
+            print("Run this script at least once in a week to get all the records!")
+
+
+if __name__ == "__main__":
+    if not os.path.exists(FILE_NAME):
+        print("It looks like this is the first time you run this script!")
+        print("This is the start date: {0}".format(START_DATE))
+        start_date = datetime.strptime(START_DATE, "%Y-%m-%d").date()
+        df = pd.DataFrame(columns=["date", "project", "duration"])
+        write_data_to_dataframe(df, start_date, date.today())
+        df.to_csv(FILE_NAME)
+    else:
+        df = pd.DataFrame.from_csv(FILE_NAME, header=0)
+        # Here we don't need start_date because it is calculated from previous entries
+        write_data_to_dataframe(df, START_DATE, date.today())
+        df.to_csv(FILE_NAME)
+
+    print("Data collection stopped!")
